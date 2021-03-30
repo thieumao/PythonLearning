9e0d22cdcc1649da15f5513c18220af79302097a
Thieu Nguyen <thieu.nguyen@niteco.se>
2021-03-30 14:11:07 +0700

    Auto Login Facebook

--- demoAutoTest.py
+++ demoAutoTest.py
@@ -1,11 +1,21 @@
 from selenium import webdriver
 from time import sleep
+from selenium.webdriver.common.keys import Keys
 
 # 1. Declare browser
 browser = webdriver.Chrome(executable_path="./chromedriver89")
 
 # 2. Open Website
 browser.get("https://www.facebook.com/")
+
+txtUser = browser.find_element_by_id("email")
+txtUser.send_keys("YourEmail")
+
+txtPass = browser.find_element_by_id("pass")
+txtPass.send_keys("YourPassword")
+
+txtPass.send_keys(Keys.ENTER)
+
 
 # 3. Stop 5 seconds
 sleep(5)
