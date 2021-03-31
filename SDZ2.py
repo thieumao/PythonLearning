23c847a88f8084d14b4c0e588d1c5c410a262d70
Thieu Nguyen <thieu.nguyen@niteco.se>
2021-03-31 17:59:49 +0700

    Need to continue

--- SDZ2.py
+++ SDZ2.py
@@ -1,10 +1,29 @@
-import MetaTrader5 as mt5
+# import MetaTrader5 as mt5
 import time
 
-mt5.initialize()
+# mt5.initialize()
 
 symbol = 'XAUUSD'
 
-while(True):
+count = 0
+
+def isDemandZone():
+    return count % 2 == 0
+
+def isSupplyZone():
+    return count % 2 == 1
+
+def buy():
+    print('Buy Action')
+
+def sell():
+    print('Sell Action')
+
+while (True):
     time.sleep(0.5)
-    print(mt5.symbol_info_tick(symbol).last)
+    count = count + 1
+    if isDemandZone():
+        buy()
+    elif isSupplyZone():
+        sell()
+    # print(mt5.symbol_info_tick(symbol).last)
